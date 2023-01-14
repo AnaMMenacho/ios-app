a47591ef16708d95da46d67af385512129ae681f
AnaMartinezM <anamarmen90@gmail.com>
2023-01-14 19:49:49 +0100

    chore: add typescript

--- App.js
+++ App.js
@@ -1,18 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native';
+import Main from "./src/containers/Main";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <Main />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/containers/Main/index.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from "react-native";
+
+const Main = () => {
+return(
+    <View>
+<Text>Hola</Text>
+    </View>
+)
+}
+
+export default Main
